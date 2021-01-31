42eaac0bdb320ff9f6c515f2c53d558c95977c58
Taylor Monacelli <taylormonacelli@gmail.com>
2021-01-31 06:44:21 -0800

    [skip ci] this should work on a machine somewhere

--- nuget.ps1
+++ nuget.ps1
@@ -1,5 +1,5 @@
 $nuget = Get-PackageProvider -Force -ErrorAction SilentlyContinue -Name NuGet
 
-if(!$nuget){
+if(!$nuget) {
     Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 }
