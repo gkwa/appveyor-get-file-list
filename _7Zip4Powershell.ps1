00bbb513e631e6886d246a619b5eedb0f1889ac9
Taylor Monacelli <taylormonacelli@gmail.com>
2021-01-31 06:26:45 -0800

    real men dont make git commit comments

--- _7Zip4Powershell.ps1
+++ _7Zip4Powershell.ps1
@@ -1,3 +1,4 @@
 if (!(Get-Module -ListAvailable -Name 7Zip4Powershell)) {
-    Install-Module -Verbose -Name 7Zip4Powershell
+#     Install-Module -Verbose -Name 7Zip4Powershell
+    Install-Module -Name 7Zip4Powershell
 }
