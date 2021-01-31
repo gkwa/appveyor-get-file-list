a071790c3fcb8b253415b15c77d0535994c9efb1
Taylor Monacelli <taylormonacelli@gmail.com>
2021-01-31 06:23:33 -0800

    real men dont make git commit comments

--- psgallery.ps1
+++ psgallery.ps1
@@ -1,4 +1,5 @@
 $psgallery = Get-PSRepository -ErrorAction SilentlyContinue -Name PSGallery
 if(!$psgallery.Trusted) {
-    Set-PSRepository -Verbose -Name PSGallery -InstallationPolicy Trusted
+#    Set-PSRepository -Verbose -Name PSGallery -InstallationPolicy Trusted
+    Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 }
