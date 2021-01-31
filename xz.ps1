b9dd08ec99d2d490e6c128506cfa5758c1233b92
Taylor Monacelli <taylormonacelli@gmail.com>
2021-01-31 06:40:07 -0800

    real men dont make git commit comments

--- xz.ps1
+++ xz.ps1
@@ -3,8 +3,11 @@ $url = [System.Uri]$url
 $wc = New-Object System.Net.WebClient
 $filename = Split-Path -Leaf -Path $url.AbsolutePath
 MkDir -Force output | Out-Null
+
 $extract_to = Join-Path output $filename
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
 if (!(Test-Path($extract_to))) { $wc.DownloadFile($url.OriginalString, $extract_to) }
+
 Expand-7Zip $extract_to -TargetPath output
-$env:PATH += ';output\bin_x86-64'
+$abs = Join-Path $pwd output\bin_x86-64
+$env:PATH += ";$abs"
