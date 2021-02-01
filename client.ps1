b414e37b3c160a08eb0400922ced1d24f590db37
Taylor Monacelli <taylormonacelli@gmail.com>
2021-01-31 21:55:19 -0800

    real men dont make git commit comments

--- client.ps1
+++ client.ps1
@@ -8,4 +8,4 @@ $zip2 = Join-Path $pwd all.xz
 Get-ChildItem C:\ -Recurse -Force -ErrorAction SilentlyContinue | Select -exp fullname >all.txt
 
 dir all.txt | Compress-7Zip -CompressionLevel Ultra -ArchiveFileName $zip1
-xz --stdout --keep --extreme --verbose all.txt >$zip2
+# xz --stdout --keep --extreme --verbose all.txt >$zip2
