48d7d220ee409c50cedac540bcedcbb18ac3fe80
Taylor Monacelli <taylormonacelli@gmail.com>
2021-02-03 05:20:11 -0800

    real men dont make git commit comments

--- client.ps1
+++ client.ps1
@@ -7,5 +7,6 @@ $zip2 = Join-Path $pwd all.xz
 
 Get-ChildItem C:\ -Recurse -Force -ErrorAction SilentlyContinue | Select -exp fullname >all.txt
 
-dir all.txt | Compress-7Zip -CompressionLevel Ultra -ArchiveFileName $zip1
+# disabled: can't complete within appveyor's 1 hour limit
+# dir all.txt | Compress-7Zip -CompressionLevel Ultra -ArchiveFileName $zip1
 # xz --stdout --keep --extreme --verbose all.txt >$zip2
